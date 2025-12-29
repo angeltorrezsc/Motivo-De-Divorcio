82c132e0c425d718db0085ca043856e3f86c24a0
ANgel <luisangeltorrezuyuquipa@gmail.com>
2025-12-29 14:29:24 -0400

    Proyecto Next.js estático listo para deploy

---
+++ .next/static/chunks/app/layout-9ddcc86470796092.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3718:function(n,e,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,6050,23))},6050:function(){}},function(n){n.O(0,[387,972,971,117,744],function(){return n(n.s=3718)}),_N_E=n.O()}]);
